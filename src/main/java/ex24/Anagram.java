82fd3d1371d33aafa38bae052463db633c0c1534
Rey <reyfundora@icloud.com>
2021-06-08 14:27:24 -0400

    Learning classes!

--- src/main/java/ex24/Anagram.java
+++ src/main/java/ex24/Anagram.java
@@ -1,10 +1,21 @@
 package ex24;
 
 import java.util.Arrays;
+import java.util.Scanner;
 
 
 class Anagram {
     static String first, second, output = "The two words are not anagrams.";
+
+    static void user_input() {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Enter two strings and I'll tell you if they are anagrams!\n" +
+                "Enter the first string: ");
+        Anagram.first = input.nextLine();
+        System.out.print("Enter the second string: ");
+        Anagram.second = input.nextLine();
+    }
 
     static boolean isSameLength() {
         int length_first = first.length();
@@ -13,7 +24,6 @@ class Anagram {
     }
 
     static boolean isAnagram() {
-        String output = "The two words are not anagrams.";
         String yes = "\"" + first + "\"" + " and " + "\"" + second + "\"" + " are anagrams.";
         String error = "\nAnagrams have to be the same length. Try again.\n";
 
@@ -29,6 +39,7 @@ class Anagram {
                 if (first_abc[i] != second_abc[i]) break;
                 else output = yes;
             }
+            System.out.println(output);
             return true;
 
         } else System.out.println(error);
