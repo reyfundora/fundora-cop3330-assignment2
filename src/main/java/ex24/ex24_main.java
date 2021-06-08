85b0f50c94020a659dd6fc83e88a70b169edfc86
Rey <reyfundora@icloud.com>
2021-06-08 14:58:22 -0400

    Learning classes!

--- src/main/java/ex24/ex24_main.java
+++ src/main/java/ex24/ex24_main.java
@@ -1,7 +1,12 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Rey Fundora
+ */
 
 package ex24;
 
 import java.util.Scanner;
+
 
 public class ex24_main {
     static void user_input() {
@@ -15,7 +20,8 @@ public class ex24_main {
     }
 
     public static void main(String[] args) {
-        do user_input();
-        while (!Anagram.isAnagram());
+        do {
+            user_input();
+        } while (!Anagram.isAnagram());
     }
 }
