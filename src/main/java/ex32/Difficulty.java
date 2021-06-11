a62222bbc0adc3128a88d098b0271ed5c642739c
Rey <reyfundora@icloud.com>
2021-06-11 17:14:10 -0400

    ex31 done

---
+++ src/main/java/ex32/Difficulty.java
@@ -0,0 +1,14 @@
+package ex32;
+
+public class Difficulty {
+    static void level_one() {
+        System.out.println("yes");
+    }
+
+    static void level_two() {
+    }
+
+    static void level_three() {
+    }
+
+}
--- src/main/java/ex32/main32.java
+++ src/main/java/ex32/main32.java
@@ -1,4 +1,30 @@
 package ex32;
 
+import java.util.Scanner;
+
 public class main32 {
+
+    public static void main (String[] args) {
+
+        while(true) {
+            Scanner input = new Scanner(System.in);
+
+            System.out.print("Let's play Guess the Number!\n" +
+                    "Enter the difficulty level (1, 2, or 3): ");
+            String level = input.next();
+
+            try {
+                int level_num = Integer.parseInt(level);
+                if (level_num == 1) Difficulty.level_one();
+                if (level_num == 2) Difficulty.level_two();
+                if (level_num == 3) Difficulty.level_three();
+                if (level_num > 3) throw new Exception();
+                break;
+            }
+
+            catch (Exception e) {
+                System.out.println("Invalid input. For difficulty, only enter 1, 2, or 3. Try again.\n");
+            }
+        }
+    }
 }
