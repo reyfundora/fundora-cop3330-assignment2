784bcd3478cbf90a20408e2302c9ce6b3867f68b
Rey <reyfundora@icloud.com>
2021-06-11 18:40:19 -0400

    34 doine

--- src/main/java/ex34/main34.java
+++ src/main/java/ex34/main34.java
@@ -1,4 +1,12 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 2 Solution
+ *  Copyright 2021 Rey Fundora
+ */
+
 package ex34;
 
 public class main34 {
+    public static void main (String[] args) {
+
+    }
 }
