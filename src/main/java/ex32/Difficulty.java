9d99eec9f8df64bdc051e7e05715e001de363a0a
Rey <reyfundora@icloud.com>
2021-06-11 18:23:01 -0400

    ex32 done

--- src/main/java/ex32/Difficulty.java
+++ src/main/java/ex32/Difficulty.java
@@ -1,18 +1,42 @@
 package ex32;
 
+import java.util.Scanner;
+
 import static ex32.main32.level_num;
 
 public class Difficulty {
     static void start_game() {
-        int min = 0, max = 0;
+        Scanner input = new Scanner(System.in);
+        String distance = "";
+
+        int count = 1, min = 0, max = 0;
         if (level_num == 1) { min = 1; max = 10; }
         if (level_num == 2) { min = 1; max = 100; }
         if (level_num == 3) { min = 1; max = 1000; }
         int random_num = (int)Math.floor(Math.random() * (max - min + 1) + min);
 
+        try {
+            System.out.print("I have my number. What's your guess? ");
+            String guess_in = input.next();
+            int guess = Integer.parseInt(guess_in);
 
+            if (guess == random_num) System.out.println("You got it on your first try!\n");
+            else {
+               while(guess != random_num) {
+                   if (guess < random_num) distance = "Too low. ";
+                   if (guess > random_num) distance = "Too high. ";
 
+                   System.out.print(distance + "Guess again: ");
+                   guess_in = input.next();
+                   guess = Integer.parseInt(guess_in);
+                   count++;
+               }
+            }
+            if (count != 1) System.out.printf("You got it in %d guesses!", count);
+        }
+
+        catch (Exception e) {
+            System.out.println("Invalid input. Please only enter integer values for your guesses. Try again.");
+        }
     }
-
-
 }
--- src/main/java/ex32/main32.java
+++ src/main/java/ex32/main32.java
@@ -4,24 +4,32 @@ import java.util.Scanner;
 
 public class main32 {
     static int level_num;
-    public static void main (String[] args) {
 
-        while(true) {
+    public static void main(String[] args) {
+
+        while (true) {
             Scanner input = new Scanner(System.in);
 
             System.out.print("Let's play Guess the Number!\n" +
                     "Enter the difficulty level (1, 2, or 3): ");
-            String level = input.next();
+            String level = input.nextLine();
 
             try {
                 level_num = Integer.parseInt(level);
-                if (0 < level_num && level_num < 3) Difficulty.start_game();
+                if (0 < level_num && level_num < 4) Difficulty.start_game();
                 else if (level_num > 3) throw new Exception();
-                break;
+            } catch (Exception e) {
+                System.out.print("Invalid input. For difficulty, only enter 1, 2, or 3. Try again.");
             }
 
-            catch (Exception e) {
-                System.out.println("Invalid input. For difficulty, only enter 1, 2, or 3. Try again.\n");
+            while(true) {
+                System.out.println("\n");
+                System.out.print("Do you wish to play again (Y/N)? ");
+                String again = input.nextLine();
+
+                if (again.equals("y") || again.equals("Y")) break;
+                if (again.equals("n") || again.equals("N")) return;
+                else System.out.print("Invalid input. Only enter Y or N for previous question.");
             }
         }
     }
