5f7106b96483fc3839bd7da9b7acdce93457386b
Rey <reyfundora@icloud.com>
2021-06-11 17:27:59 -0400

    ex31 done

--- src/main/java/ex32/Difficulty.java
+++ src/main/java/ex32/Difficulty.java
@@ -1,14 +1,18 @@
 package ex32;
 
+import static ex32.main32.level_num;
+
 public class Difficulty {
-    static void level_one() {
-        System.out.println("yes");
+    static void start_game() {
+        int min = 0, max = 0;
+        if (level_num == 1) { min = 1; max = 10; }
+        if (level_num == 2) { min = 1; max = 100; }
+        if (level_num == 3) { min = 1; max = 1000; }
+        int random_num = (int)Math.floor(Math.random() * (max - min + 1) + min);
+
+
+
     }
 
-    static void level_two() {
-    }
-
-    static void level_three() {
-    }
 
 }
--- src/main/java/ex32/main32.java
+++ src/main/java/ex32/main32.java
@@ -3,7 +3,7 @@ package ex32;
 import java.util.Scanner;
 
 public class main32 {
-
+    static int level_num;
     public static void main (String[] args) {
 
         while(true) {
@@ -14,11 +14,9 @@ public class main32 {
             String level = input.next();
 
             try {
-                int level_num = Integer.parseInt(level);
-                if (level_num == 1) Difficulty.level_one();
-                if (level_num == 2) Difficulty.level_two();
-                if (level_num == 3) Difficulty.level_three();
-                if (level_num > 3) throw new Exception();
+                level_num = Integer.parseInt(level);
+                if (0 < level_num && level_num < 3) Difficulty.start_game();
+                else if (level_num > 3) throw new Exception();
                 break;
             }
 
