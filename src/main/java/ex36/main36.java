b19f783ab0400e7eda8c048916885f37cbf641cd
Rey <reyfundora@icloud.com>
2021-06-12 01:19:59 -0400

    35 done

--- src/main/java/ex36/main36.java
+++ src/main/java/ex36/main36.java
@@ -1,4 +1,30 @@
 package ex36;
 
+import java.util.Scanner;
+
 public class main36 {
+    static double[] list = new double[1000];
+    static double num;
+    static String output;
+
+    public static void main (String[] args) {
+        Scanner input = new Scanner(System.in);
+        int counter = 0;
+
+        while(true) {
+            System.out.print("Enter a number: ");
+            String num_in = input.nextLine();
+
+            if (num_in.equals("done")) { System.out.println(list[counter]); break; }
+            else {
+                try {
+                    num = Double.parseDouble(num_in);
+                    list[counter] = num;
+                    counter++;
+
+                }
+                catch (Exception e) { System.out.println("Please only enter numerical values. Try again."); }
+            }
+        }
+    }
 }
