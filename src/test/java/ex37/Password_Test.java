748e3088c1e67766ea46c53db5162dff4be99d50
Rey <reyfundora@icloud.com>
2021-06-13 15:53:53 -0400

    Password done!

--- src/test/java/ex37/Password_Test.java
+++ src/test/java/ex37/Password_Test.java
@@ -5,12 +5,49 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class Password_Test {
     @Test
-    void is_average_working() {
+    void is_password_working() {
         //given
+        main37.min_length = 8;
+        main37.spl_length = 2;
+        main37.num_length = 2;
 
         //when
+        Password.generate();
 
-        String actual =
+        int actual = main37.min_length;
+        int expect = 9;
+
+        //then
+        assertEquals(expect, actual);
+    }
+    @Test
+    void is_password_working2() {
+        //given
+        main37.min_length = 8;
+        main37.spl_length = 6;
+        main37.num_length = 6;
+
+        //when
+        Password.generate();
+
+        int actual = main37.min_length;
+        int expect = 25;
+
+        //then
+        assertEquals(expect, actual);
+    }
+    @Test
+    void is_password_working3() {
+        //given
+        main37.min_length = 8;
+        main37.spl_length = 0;
+        main37.num_length = 0;
+
+        //when
+        Password.generate();
+
+        int actual = main37.min_length;
+        int expect = 8;
 
         //then
         assertEquals(expect, actual);
--- src/test/java/ex37/main_test.java
+++ src/test/java/ex37/main_test.java
@@ -1,4 +1,39 @@
 package ex37;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class main_test {
+    @Test
+    void is_error_working() {
+        //given
+        String output = "";
+        String length_in = "5";
+        String special_in = "5";
+        String num_in = "r";
+
+        //when
+        try {
+            int min_length = Integer.parseInt(length_in);
+            int spl_length = Integer.parseInt(special_in);
+            int num_length = Integer.parseInt(num_in);
+        }
+        catch (Exception e){
+            output = """
+                        
+                 INVALID INPUT
+                 Only enter whole numbers when prompted. Try again.
+                 """;
+        }
+
+        String actual = output;
+        String expect = """
+                        
+                 INVALID INPUT
+                 Only enter whole numbers when prompted. Try again.
+                 """;
+
+        //then
+        assertEquals(expect, actual);
+    }
 }
