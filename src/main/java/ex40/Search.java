8786d9c78fc84db2023d295bdd1cbf81bad2b86c
Rey <reyfundora@icloud.com>
2021-06-13 22:40:26 -0400

    All DONE

--- src/main/java/ex40/Search.java
+++ src/main/java/ex40/Search.java
@@ -5,24 +5,58 @@ import static ex40.HashMaps.*;
 import static ex40.main40.search;
 
 public class Search {
-    static int Jackson, Jacobson, Johnson, Michaelson, Weber, Xiong;
+
     static void Search_Function() {
+        int Jackson= 0, Jacobson = 0, Johnson = 0, Michaelson = 0, Weber = 0, Xiong = 0;
 
         String[] names = new String[6];
-        names [0] = "Jacquelyn" + "Jackson";
-        names [1] = "Jake" + "Jacobson";
-        names [2] = "John" + "Johnson";
-        names [3] = "Michaela" + "Michaelson";
-        names [4] = "Sally" + "Weber";
-        names [5] = "Tou" + "Xiong";
+        names[0] = "Jacquelyn" + "Jackson";
+        names[1] = "Jake" + "Jacobson";
+        names[2] = "John" + "Johnson";
+        names[3] = "Michaela" + "Michaelson";
+        names[4] = "Sally" + "Weber";
+        names[5] = "Tou" + "Xiong";
 
-        for (int i = 0; i < search.length(); i++) {
-            if (search.charAt(i) == names[0].charAt(i)) Jackson++;
-            if (search.charAt(i) == names[1].charAt(i)) Jacobson++;
-            if (search.charAt(i) == names[2].charAt(i)) Johnson++;
-            if (search.charAt(i) == names[3].charAt(i)) Michaelson++;
-            if (search.charAt(i) == names[4].charAt(i)) Weber++;
-            if (search.charAt(i) == names[5].charAt(i)) Xiong++;
+        int a = 0, b =0, c = 0, d = 0, e = 0, f =0;
+        for (int i = 0; i < 18; i++) {
+
+            while (a < search.length() && i < names[0].length()) {
+                if (search.charAt(a) == names[0].charAt(i)) {
+                        Jackson++; a++;
+                }
+                break;
+                }
+            while (b < search.length() && i < names[1].length()) {
+                if (search.charAt(b) == names[1].charAt(i)) {
+                    Jacobson++; b++;
+                }
+                break;
+            }
+            while (c < search.length() && i < names[2].length()) {
+                if (search.charAt(c) == names[2].charAt(i)) {
+                    Johnson++; c++;
+                }
+                break;
+            }
+            while (d < search.length() && i < names[3].length()) {
+                if (search.charAt(d) == names[3].charAt(i)) {
+                    Michaelson++; d++;
+                }
+                break;
+            }
+            while (e < search.length() && i < names[4].length()) {
+                if (search.charAt(e) == names[4].charAt(i)) {
+                    Weber++; e++;
+                }
+                break;
+            }
+            while (f < search.length() && i < names[5].length()) {
+                if (search.charAt(f) == names[5].charAt(i)) {
+                    Xiong++; f++;
+                }
+                break;
+            }
+
         }
 
         Integer[] counts = new Integer[6];
